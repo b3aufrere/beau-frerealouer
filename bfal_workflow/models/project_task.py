16c2ac515a54c8361fa25d78373b7e9872efb3f1
Kamel Benchehida <kamel8007@gmail.com>
2023-01-28 21:33:54 +0100

    module Beau-frere a louer Workflow >>> cacher Tab Sous-taches  et button 'Creer sous-tache' quand c est une sous tache

--- bfal_workflow/models/project_task.py
+++ bfal_workflow/models/project_task.py
@@ -25,7 +25,6 @@ class ProjectTask(models.Model):
                     'context': {
                         'default_user_ids': [(4, [user_id.id]) for user_id in self.user_ids] if self.user_ids else False,
                         'default_sale_order_id': self.sale_order_id.id if self.sale_order_id else False,
-                        'default_project_id': self.project_id.id if self.project_id else False,
                         'default_partner_id': self.partner_id.id if self.partner_id else False,
                         'default_company_id': self.company_id.id if self.company_id else False,
                         'default_parent_id': self.id,
