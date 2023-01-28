47ecb1bf378137a87815cd2a0d2f871675466b0d
Kamel Benchehida <kamel8007@gmail.com>
2023-01-28 20:15:58 +0100

    module Beau-frere a louer Workflow >>> ajout button 'Créer sous-tâche' a partir de la tache parent

--- bfal_workflow/models/project_task.py
+++ bfal_workflow/models/project_task.py
@@ -6,17 +6,22 @@ class ProjectTask(models.Model):
     _inherit = 'project.task'
 
     def create_sub_task(self):
-        action = self.env["ir.actions.actions"]._for_xml_id("industry_fsm.project_task_action_fsm")
-        if action:
-            action['context'] = {
-                'default_user_ids': [(4, [user_id.id]) for user_id in self.user_ids] if self.user_ids else False,
-                'default_sale_order_id': self.sale_order_id.id if self.sale_order_id else False,
-                'default_project_id': self.project_id.id if self.project_id else False,
-                'default_partner_id': self.partner_id.id if self.partner_id else False,
-                'default_company_id': self.company_id.id if self.company_id else False,
-                'default_parent_id': self.id,                     
-            }
+        view_id = self.env["ir.ui.view"]._for_xml_id("project.view_task_form2")
 
-            action['view_mode'] = 'form'
-            
-            return action
+        if view_id:
+            return {
+                    'name': _("My Tasks"),
+                    'type': 'ir.actions.act_window',
+                    'res_model': 'project.task',
+                    'view_mode': 'form',
+                    'view_id': view_id.id,
+                    'context': {
+                        'default_user_ids': [(4, [user_id.id]) for user_id in self.user_ids] if self.user_ids else False,
+                        'default_sale_order_id': self.sale_order_id.id if self.sale_order_id else False,
+                        'default_project_id': self.project_id.id if self.project_id else False,
+                        'default_partner_id': self.partner_id.id if self.partner_id else False,
+                        'default_company_id': self.company_id.id if self.company_id else False,
+                        'default_parent_id': self.id,   
+                    },
+                    'target': 'current',
+                }
