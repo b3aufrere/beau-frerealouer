c4934aeaa179be99dbef87bb619d7a9f23f80ac4
Kamel Benchehida <kamel8007@gmail.com>
2023-01-29 11:05:26 +0100

    module Beau-frere a louer Workflow >>> relié la tache crée a partir de SO a la tache de rendez vous

--- bfal_workflow/models/sale_order_line.py
+++ bfal_workflow/models/sale_order_line.py
@@ -17,5 +17,11 @@ class SaleOrderLine(models.Model):
                 stage_id = self.env['project.task.type'].search([('name', '=', 'Assigné'), ('project_ids', 'in', project.id)])
                 if stage_id:
                     values['stage_id'] = stage_id.id
+        
+        if self.order_id.task_id:
+            values['territory_id'] = self.order_id.task_id.territory_id.id if self.order_id.task_id.territory_id else False
+            values['user_ids'] = [(6, 0, self.order_id.task_id.user_ids.ids)] if self.order_id.task_id.user_ids else False
+            values['parent_id'] = self.order_id.task_id.id
 
         return values
+    
