075aa5d26224b847c4df7a8642b5eae2c10e050b
Kamel Benchehida <kamel8007@gmail.com>
2023-02-02 17:04:42 +0100

    module Beau-frere a louer Workflow >>> ajout popup d activité pour le non acceptation de tache

--- bfal_workflow/models/project_task.py
+++ bfal_workflow/models/project_task.py
@@ -27,7 +27,7 @@ class ProjectTask(models.Model):
                         'default_date_deadline': date.today(),
                         'default_user_id': self.create_uid.id,
                         'default_res_model_id': self.env.ref("mail.model_mail_activity").id,
-                        'default_res_id': self.id     
+                        'default_res_id': self     
                     },
                     'target': 'new',
                 }
