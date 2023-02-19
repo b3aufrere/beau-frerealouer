9baf6b168d75230f98a5bc480e4450583631067a
b3aufrere <118748320+b3aufrere@users.noreply.github.com>
2023-02-19 01:03:35 -0500

    Upload New Commission app

---
+++ sales_commission_generic/sales_commission_bundle_odoo/__manifest__.py
@@ -0,0 +1,51 @@
+# Part of Probuse Consulting Service Pvt Ltd. See LICENSE file for full copyright and licensing details.
+
+# -*- coding: utf-8 -*-
+{
+
+    'name': 'Sales Commission Bundle Apps',
+    'version': '3.1.4',
+    'category': 'Sales/Sales',
+    'license': 'Other proprietary',
+    'price': 8.0,
+    'currency': 'EUR',
+    'images': ['static/description/scba.jpg'],
+#     'live_test_url': 'https://youtu.be/YCTQ81S--4Y',
+    'support': 'contact@probuse.com',
+    'author': 'Probuse Consulting Service Pvt. Ltd.',
+    'website': 'www.probuse.com',
+    'summary': 'This app provide package for sales commission modules.',
+    'description' : '''
+Sales Commission Bundle
+Sales Commission External User
+Sales Commission External User Extend
+Sales Commission Web Portal
+sales commission
+sales commission user
+user commission
+agent commission
+partner Commission
+sales agent Commission
+sales bonus
+Commission
+sale Commission
+agent Commissions
+sales user Commission
+sale Commissions
+Commission portal
+customer portal Commission
+Commission account
+account Commission
+''',
+    'depends': ['sales_commission_external_user',
+                'commission_external_user_extend',
+                'sales_commission_external_agent_portal',
+                ],
+    'data': [
+            
+             ],
+    'installable': True,
+    'auto_install': False
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ sales_commission_generic/sales_commission_external_agent_portal/controllers/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Part of Probuse Consulting Service Pvt Ltd. See LICENSE file for full copyright and licensing details.
+
+from . import main
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
