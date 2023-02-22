06cffd2adf641879a8a0274bd6c92973b8a600e5
Kamel Benchehida <kamel8007@gmail.com>
2023-02-22 13:07:44 +0100

    branch >> remplacer l ancienne gestion des entreprises par la nouvelle

--- bfal_workflow/models/hr_employee.py
+++ bfal_workflow/models/hr_employee.py
@@ -11,9 +11,15 @@ class HrEmployee(models.Model):
         groups="hr.group_hr_user"
     )
 
+    branch_id = fields.Many2one(
+        'res.branch',
+        string='Entreprise',
+        groups="hr.group_hr_user"
+    )
+
     territory_id = fields.Many2one(
         'territory',
         string='Territoire de travail',
         groups="hr.group_hr_user",
-        domain="[('entreprise_ids', 'in', entreprise_id)]"
+        domain="[('branch_ids', 'in', branch_id)]"
     )
