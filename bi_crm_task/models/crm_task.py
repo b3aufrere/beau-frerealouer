19ad9ab27ec06d29628fcf732e80ca350c153c7b
Kamel Benchehida <kamel8007@gmail.com>
2023-03-11 22:00:53 +0100

    ajout nv changements au module bi_crm_task

--- bi_crm_task/models/crm_task.py
+++ bi_crm_task/models/crm_task.py
@@ -11,7 +11,7 @@ import logging
 from odoo.osv import  osv
 from odoo import SUPERUSER_ID
 
-
+from logging import warning as w
 
 class crm_lead(models.Model):
     """ CRM Lead Case """
@@ -36,28 +36,37 @@ class crm_task_wizard(models.TransientModel):
         return name
     
     
-    project_id = fields.Many2one('project.project','Project')
-    dead_line = fields.Date('Deadline')
-    name = fields.Char('Task Name',default = get_name)
-    user_ids = fields.Many2many('res.users','Assignees',default=lambda self: self.env.uid,
-        index=True)
-    user_ids = fields.Many2many('res.users', relation='project_task_assignee_rel', column1='task_id', column2='user_id',
-        string='Assignees', default=lambda self: self.env.user)
+    project_id = fields.Many2one('project.project','Projet')
+    # dead_line = fields.Date('Deadline')
+    planned_date_begin = fields.Datetime(string="Date de début")
+    planned_date_end = fields.Datetime(string="Date de fin")
+    name = fields.Char('Nom de la tâche',default = get_name)
+    # user_ids = fields.Many2many('res.users',string='Assignés')
+    # user_ids = fields.Many2many('res.users', relation='project_task_assignee_rel', column1='task_id', column2='user_id',
+    #     string='Assignees', default=lambda self: self.env.user)
 
     def create_task(self):
         ctx = dict(self._context or {})
         active_id = ctx.get('active_id')
         crm_brw = self.env['crm.lead'].browse(active_id)
         user = []
-        for users in self.user_ids:
-            user.append(users.id)
-        vals = {'name': self.name,
-                'project_id':self.project_id.id or False,
-                'user_ids': user or False,
-                'date_deadline':  self.dead_line or False,
-                'partner_id': crm_brw.partner_id.id or False,
-                'lead_id': crm_brw.id or False
-                }
+        
+        # for users in self.user_ids:
+        #     user.append(users.id)
+        
+        vals = {
+            'name': self.name,
+            'project_id':self.project_id.id or False,
+            # 'user_ids': user or False,
+            # 'date_deadline':  self.dead_line or False,
+            'planned_date_begin':  self.planned_date_begin,
+            'planned_date_end':  self.planned_date_end,
+            'partner_id': crm_brw.partner_id.id or False,
+            'lead_id': crm_brw.id or False,
+            'branch_id': crm_brw.branch_id.id or False,
+            'user_ids': [(6, 0, [crm_brw.user_id.id])] if crm_brw.user_id else False
+        }
+        
         self.env['project.task'].create(vals)
         
 class project_Task(models.Model):
