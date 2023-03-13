0f3f97ea23ffe9ea7c92461179a26725352822ed
Kamel Benchehida <kamel8007@gmail.com>
2023-03-13 20:19:21 +0100

    bfal_workflow >> envoi d mail ou sms si la template est selectioné dans l etat de lead en cas de changement vers cette etat

--- bfal_workflow/models/crm_stage.py
+++ bfal_workflow/models/crm_stage.py
@@ -6,5 +6,5 @@ class CrmStage(models.Model):
     _inherit = 'crm.stage'
 
 
-    mail_template_id = fields.Many2one('mail.template', string="Modèle d'email")
+    mail_template_id = fields.Many2one('mail.template', string="Modèle d'email", domain=[('model', '=', 'crm.lead')])
     sms__template_id = fields.Many2one("twilio.sms.template", "Modèle de SMS", domain="[('model_id', '!=', False), ('model_id.model', '=', 'crm.lead')]", copy=False)
