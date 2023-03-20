0d67c17a7b52c386a259ff9098fe930aeb3b9db6
Kamel Benchehida <kamel8007@gmail.com>
2023-03-20 11:36:36 +0100

    enlever gestion des branches

--- bfal_workflow/models/account_move.py
+++ bfal_workflow/models/account_move.py
@@ -4,7 +4,7 @@ from odoo import models, fields
 class AccountMove(models.Model):
     _inherit = "account.move"
 
-    division_id = fields.Many2one('division', related='invoice_user_id.employee_id.branch_id.division_id', string='Division')
+    # division_id = fields.Many2one('division', related='invoice_user_id.employee_id.branch_id.division_id', string='Division')
     # entreprise_id = fields.Many2one('entreprise', related='invoice_user_id.employee_id.entreprise_id', string='Entreprise')
     # branch_id = fields.Many2one('res.branch', related='invoice_user_id.employee_id.branch_id', string="Entreprise")
 
