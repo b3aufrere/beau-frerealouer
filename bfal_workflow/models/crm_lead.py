db62cd82edbc5cdbb635277a9b35740b56042c4e
Kamel Benchehida <kamel8007@gmail.com>
2023-03-22 17:24:10 +0100

    bfal_workflow >> ajout nv champs vendeur dans lead

--- bfal_workflow/models/crm_lead.py
+++ bfal_workflow/models/crm_lead.py
@@ -21,7 +21,7 @@ class CrmLead(models.Model):
     # )
 
     user_id = fields.Many2one(
-        'res.users', string='Salesperson',
+        'res.users', string='Salesperson',default=False,
         # domain="['&', ('share', '=', False), '&', ('company_ids', 'in', user_company_ids), '&', ('employee_id', '!=', False), '&', ('employee_id.branch_id', '!=', False), ('employee_id.branch_id', '=', branch_id)]",
         domain="['&', ('share', '=', False), '&', ('company_ids', 'in', user_company_ids)]",
         check_company=True, index=True, tracking=True)
