2787c8c597f49c0f12d330962d247e93f5300369
Kamel Benchehida <kamel8007@gmail.com>
2023-04-12 23:34:52 +0100

    bfal_workflow >> ajout tableau historique des assignations dans la soumission

--- bfal_workflow/models/mail_activity.py
+++ bfal_workflow/models/mail_activity.py
@@ -8,7 +8,6 @@ class MailActivity(models.Model):
     _inherit = 'mail.activity'
 
     def action_close_dialog(self):
-        w("mail >>>>>>>> action_close_dialog 1")
         res = super(MailActivity, self).action_close_dialog
 
         if 'is_from_task' in self._context:
@@ -19,14 +18,13 @@ class MailActivity(models.Model):
                 task.stage_id = new_stage_id.id
                 
                 if task.sale_order_id:
-                    w("mail >>>>>>>> action_close_dialog 2")
                     self.env['task.assignment.history'].create({
                         'order_id': task.sale_order_id.id,
                         'task_id': task.id,
                         'motif': self.summary,
                         'user_ids': [(6, 0, [user.id for user in task.user_ids])] if task.user_ids else False
                     })
-                    
+
                 task.user_ids = False
             else:
                 raise UserError("Il faut ajouté une étape Non accepté a ce projet")
