037d1ffa8090e90a89e6f85162ce8a84762f6348
Kamel Benchehida <kamel8007@gmail.com>
2023-05-03 17:53:42 +0100

    bfal_workflow >> Ajouter une étape d'acceptation pour les soumissions avant création du bon de commande

--- bfal_workflow/models/sale_order.py
+++ bfal_workflow/models/sale_order.py
@@ -12,6 +12,7 @@ class SaleOrder(models.Model):
     # entreprise_id = fields.Many2one('entreprise', related='user_id.employee_id.entreprise_id', string='Entreprise')
     branch_id = fields.Many2one('res.branch', related='user_id.employee_id.branch_id', string='Entreprise')
     task_assignment_history_ids = fields.One2many('task.assignment.history', 'order_id', string="Historique des assignations")
+    state = fields.Selection(Selection_add=[('not_accepted', 'Non accepté')])
 
     @api.depends('meeting_ids')
     def _compute_meeting_count(self):
@@ -48,3 +49,7 @@ class SaleOrder(models.Model):
                 }
                 
                 return action
+    
+    def action_not_accepted(self):
+        self.state = "not_accepted"
+        self.user_id = False
