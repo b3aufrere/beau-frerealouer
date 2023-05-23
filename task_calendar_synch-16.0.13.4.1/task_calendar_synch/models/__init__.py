a0472dc6f84f17821043b32de8bef77a40c8afc4
Kamel Benchehida <kamel8007@gmail.com>
2023-05-23 22:24:25 +0100

    Delete task_calendar_synch-16.0.13.4.1/task_calendar_synch directory

--- task_calendar_synch-16.0.13.4.1/task_calendar_synch/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# Part of Probuse Consulting Service Pvt. Ltd. See LICENSE file for full copyright and licensing details.
-
-from . import project_task_inherited
-from . import calander_event
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
