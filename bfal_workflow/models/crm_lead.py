a5b0280fcc83639577f93c7204a54de63f547260
Kamel Benchehida <kamel8007@gmail.com>
2023-05-23 18:28:29 +0100

    bfal_workflow >> fixer probleme de non remplissage auto de entreprise dans soumission

--- bfal_workflow/models/crm_lead.py
+++ bfal_workflow/models/crm_lead.py
@@ -81,19 +81,29 @@ class CrmLead(models.Model):
         string="Rôle",
         related='stage_id.role'
     )
-    
-    def _prepare_opportunity_quotation_context(self):
-        quotation_context = super(CrmLead, self)._prepare_opportunity_quotation_context()
 
+    def action_view_sale_quotation(self):
+        action = super(CrmLead, self).action_view_sale_quotation()
         if self.branch_id:
-            quotation_context['default_branch_id'] = self.branch_id.id
+            action['context']['default_branch_id'] = self.branch_id.id
         
         if self.user_id:
-            quotation_context['default_user_id'] = self.user_id.id
+            action['context']['default_user_id'] = self.user_id.id
 
-        w(f"quotation_context >> {quotation_context}")
+        w(f"action >>> {action}")
+        
+        return action
+    
+    # def _prepare_opportunity_quotation_context(self):
+    #     quotation_context = super(CrmLead, self)._prepare_opportunity_quotation_context()
 
-        return quotation_context
+    #     if self.branch_id:
+    #         quotation_context['default_branch_id'] = self.branch_id.id
+        
+    #     if self.user_id:
+    #         quotation_context['default_user_id'] = self.user_id.id
+
+    #     return quotation_context
 
     @api.onchange('sale_order_count')
     def compute_is_accepted(self):
